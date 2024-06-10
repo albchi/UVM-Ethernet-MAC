0f0afb930eb60ff9b55ac73e16c7b3f0fe1659de
VLSIX410-Spring2024-4 <albchi@gmail.com>
2024-06-10 14:53:20 -0700

    Added agent_pkt_tx

--- testbench/uvm/env.sv
+++ testbench/uvm/env.sv
@@ -1,6 +1,8 @@
 `ifndef	_ENV_
 `define	_ENV_
 
+`include "agent_pkt_tx.sv"
+
 `include "agent_reset.sv"
 `include "sequence_reset.sv"
 
@@ -14,6 +16,7 @@ class env extends uvm_env;
    // input_agent agent_in;
    agent_reset agent_reset_0; 
    agent_wb agent_wb_0; 
+   agent_pkt_tx agent_pkt_tx_0; 
 
    function new(string name = "10 GBEth Env", uvm_component parent);
       super.new(name,parent);
@@ -23,6 +26,7 @@ class env extends uvm_env;
       super.build_phase(phase);
       agent_reset_0 = agent_reset::type_id::create("agent_reset_0", this);
       agent_wb_0 = agent_wb::type_id::create("agent_wb_0", this);
+      agent_pkt_tx_0 = agent_pkt_tx::type_id::create("agent_pkt_tx_0", this);
    endfunction 
 
 endclass
